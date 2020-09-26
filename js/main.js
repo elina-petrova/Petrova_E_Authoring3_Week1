4510aeda4694a2d6704480f37b66b0fbf99e5411
elina <e_petrova@fanshaweonline.ca>
2020-09-26 18:14:29 -0400

    jsmodules

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// import your package here
+import Person from "./modules/DataModule.js";
+import NavSystem from "./modules/TheNavSystem.js";
+
+(()=>{
+    //stub * just a place for non-component-specific files
+    console.log('loaded');
+
+    console.log(Person);
+})();
---
+++ js/modules/TheNavSystem.js
@@ -0,0 +1,3 @@
+module.exports = {
+    message: "i an the nav module"
+}
