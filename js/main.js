4a4683fb95e33b5fd384f15b7600e71c5633d9d8
elina <e_petrova@fanshaweonline.ca>
2020-10-14 17:48:44 -0400

    adding dynamic content

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
 // import your package here
-import Person from "./modules/DataModule.js";
+import Team from "./modules/DataModule.js";
 import NavSystem from "./modules/TheNavSystem.js";
 
 (()=>{
     //stub * just a place for non-component-specific files
     console.log('loaded');
 
-    console.log(Person);
+    console.log(Team);
+
+    //select our user elements and load the content 
+    let userSection = document.querySelector(".user-section"),
+        userTemplate = document.querySelector("#profs-template").content;
+
+    
+    // 
+    function handleDataSet(data){
+        for (let user in data){
+            let currentUser = userTemplate.cloneNode(true),
+                currentUserText = currentUser.querySelector('.user').children;
+            
+                currentUserText[1].textContent = data[user].name;
+                currentUserText[2].textContent = data[user].role;
+                currentUserText[3].textContent = data[user].nickname;
+
+                userSection.appendChild(currentUser);
+        }
+    }
+    handleDataSet(Team);
+
+
 })();
--- js/modules/DataModule.js
+++ js/modules/DataModule.js
@@ -1,5 +1,22 @@
 export default {
-    name: "Trevor",
-    role: "Prof",
-    nickname: "Trevor"
+    Justin: {
+    name: "Sa",
+    role: "ini",
+    nickname: "Tvor"
+    },
+    John: {
+        name: "John",
+        role: "super chill prof",
+        nickname: "just john"
+    },
+    Joe: {
+        name: "Joe",
+        role: "Prof",
+        nickname: "Teddy Bear"
+    },
+    Pan: {
+        name: "Pan",
+        role: "Code Ninja",
+        nickname: "Spider Pan"
+    }
 }
