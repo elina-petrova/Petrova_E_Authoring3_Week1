5940ba981283e7f4b8ecd5c451de363b46524248
elina <e_petrova@fanshaweonline.ca>
2020-10-14 19:28:18 -0400

    adding AJAX

--- js/main.js
+++ js/main.js
@@ -4,6 +4,39 @@ import NavSystem from "./modules/TheNavSystem.js";
 
 (()=>{
     //stub * just a place for non-component-specific files
+
+    //set up the XMLHHTP object
+    let myReq = new XMLHttpRequest;
+
+    //make sure we can handle whatever data comes back, or any errors
+    myReq.addEventListener("readystatechange", handleRequest);
+
+    //open a requst and pass thru the URL of the data that we want
+    myReq.open('GET', '../DataSet.json');
+
+    //actually make the request
+    myReq.send();
+    
+    function handleRequest(){
+        if(myReq.readyState === XMLHttpRequest.DONE){
+            //check status here and proceed
+            if(myReq.status === 200){
+                //200 means done and dusted,ready to go with the dataset!
+                handleDataSet(JSON.parse(myReq.responseText));
+            } else {
+                //probably got some kind of error code, so handle that
+                //a 404, 500 etc...can render appropriate message here
+
+                console.error(`${myReq.status}: something done broke, son`);
+            } 
+        }else {
+                // requst isnt ready yet keep waiting
+                console.log(`Requst state: ${myReq.readyState}. Still processing`);
+            }
+        }
+    
+
+
     console.log('loaded');
 
     console.log(Team);
@@ -15,6 +48,7 @@ import NavSystem from "./modules/TheNavSystem.js";
     
     // 
     function handleDataSet(data){
+
         for (let user in data){
             let currentUser = userTemplate.cloneNode(true),
                 currentUserText = currentUser.querySelector('.user').children;
@@ -26,7 +60,7 @@ import NavSystem from "./modules/TheNavSystem.js";
                 userSection.appendChild(currentUser);
         }
     }
-    handleDataSet(Team);
+    // handleDataSet(Team);
 
 
 })();
