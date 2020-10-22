acad8cde320311ea89c10406f51554c68396cd4e
elina <e_petrova@fanshaweonline.ca>
2020-10-22 13:20:52 -0400

    done with writing fetch

--- js/main.js
+++ js/main.js
@@ -1,45 +1,20 @@
 // import your package here
-import Team from "./modules/DataModule.js";
-import NavSystem from "./modules/TheNavSystem.js";
+
 
 (()=>{
-    //stub * just a place for non-component-specific files
+    // start with a Fetch all
 
-    //set up the XMLHHTP object
-    let myReq = new XMLHttpRequest;
-
-    //make sure we can handle whatever data comes back, or any errors
-    myReq.addEventListener("readystatechange", handleRequest);
-
-    //open a requst and pass thru the URL of the data that we want
-    myReq.open('GET', '../DataSet.json');
-
-    //actually make the request
-    myReq.send();
-    
-    function handleRequest(){
-        if(myReq.readyState === XMLHttpRequest.DONE){
-            //check status here and proceed
-            if(myReq.status === 200){
-                //200 means done and dusted,ready to go with the dataset!
-                handleDataSet(JSON.parse(myReq.responseText));
-            } else {
-                //probably got some kind of error code, so handle that
-                //a 404, 500 etc...can render appropriate message here
-
-                console.error(`${myReq.status}: something done broke, son`);
-            } 
-        }else {
-                // requst isnt ready yet keep waiting
-                console.log(`Requst state: ${myReq.readyState}. Still processing`);
-            }
-        }
-    
-
+    fetch('./DataSet.json')
+        .then(res => res.json())
+        .then(data => {
+            //this is our data (DataSet.json)
+            //converted to a plain JavaScript object
+            handleDataSet(data);
+        })
+        .catch((error)=> console.log(error));
 
     console.log('loaded');
 
-    console.log(Team);
 
     //select our user elements and load the content 
     let userSection = document.querySelector(".user-section"),
@@ -52,10 +27,11 @@ import NavSystem from "./modules/TheNavSystem.js";
         for (let user in data){
             let currentUser = userTemplate.cloneNode(true),
                 currentUserText = currentUser.querySelector('.user').children;
-            
-                currentUserText[1].textContent = data[user].name;
-                currentUserText[2].textContent = data[user].role;
-                currentUserText[3].textContent = data[user].nickname;
+
+                currentUserText[1].src = `images/${data[user].avatar}.jpg`;
+                currentUserText[2].textContent = data[user].name;
+                currentUserText[3].textContent = data[user].role;
+                currentUserText[4].textContent = data[user].nickname;
 
                 userSection.appendChild(currentUser);
         }
